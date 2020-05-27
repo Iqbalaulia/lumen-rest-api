3e370338530c53b58682aa30b0c34ea1cdf778b7
Iqbalaulia <16410100012@stikom.edu>
2020-05-27 21:06:24 +0700

    implementasi API Auth login

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+
+    public function __construct(){
+        
+        $this->middleware('auth');
+    }
+
     public function index(){
         $products = Products::all();
         return response()->json($products);
